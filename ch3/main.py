5c1d07df593c125b475087f68c8ee49639ea7169
amzadhossainjacky <amzadhossainjacky@gmail.com>
2025-01-26 12:15:05 +0600

    ch3 customize

---
+++ ch3/main.py
@@ -0,0 +1,18 @@
+# from decouple import config
+from decouple import config
+SECRET_KEY = config('OPENAI_API_KEY')
+
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+model = ChatOpenAI(model="gpt-4o-mini", openai_api_key=SECRET_KEY)
+
+messages = [
+    HumanMessage("Hello"),
+]
+
+response = model.invoke(messages)
+print(response)
+
+
+
